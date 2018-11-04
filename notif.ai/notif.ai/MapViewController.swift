4153fa512e53a268153a46715c3acba49200c922
vitali <vshypko@gmail.com>
2018-11-03 18:04:36 -0700

    tekuschaia situatsia

--- notif.ai/notif.ai/MapViewController.swift
+++ notif.ai/notif.ai/MapViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import Firebase
 
 class MapViewController: UIViewController {
 
+    @IBAction func addEvent(_ sender: Any) {
+        let alertController = UIAlertController(title: "TESTTEST", message: "YOO", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
